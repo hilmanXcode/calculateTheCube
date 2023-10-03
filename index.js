ea34e4378844540969af529a51d76dda9f8c622a
Hilman.js <62336112+hilmanXcode@users.noreply.github.com>
2023-10-03 11:35:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const calculateCube = (a) => {
-  for(let i = 1; i <= a; i++)
-  {
-    console.log(`Current Number is : ${i} and the cube is ${i**3}`)
-  }
+  for(let i = 1; i <= a; i++) console.log(`Current Number is : ${i} and the cube is ${i**3}`)
 }
 
 calculateCube(6);
